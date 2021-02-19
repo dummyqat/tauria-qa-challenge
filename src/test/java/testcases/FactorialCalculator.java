684fd14751d94625a9232a2566660eec157b914e
Clarissa Macasaquit <75256692+dummyqat@users.noreply.github.com>
2021-02-19 19:00:24 +0800

    Update FactorialCalculator.java

--- src/test/java/testcases/FactorialCalculator.java
+++ src/test/java/testcases/FactorialCalculator.java
@@ -1,9 +1,6 @@
 package testcases;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -11,7 +8,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FactorialCalculator {
@@ -30,10 +26,7 @@ public class FactorialCalculator {
 
 	public static void browserSetup() {
 		WebDriverManager.chromedriver().setup();
-		Map<String, Object> prefs = new HashMap<String, Object>();
-		prefs.put("profile.default_content_setting_values.notifications", 2);
 		ChromeOptions options = new ChromeOptions();
-		options.setExperimentalOption("prefs", prefs);
 		driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
